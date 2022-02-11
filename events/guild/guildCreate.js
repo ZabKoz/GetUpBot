329649f50940ee6ab841929d12348fd856bd7458
ZabKoz <zabkoz@spoko.pl>
2022-02-11 15:03:40 +0100

    ⚙| Created adding server to MySQL database

---
+++ events/guild/guildCreate.js
@@ -0,0 +1,31 @@
+// ———————————————[Packages]———————————————
+const chalk = require('chalk');
+const client = require('../../bot');
+const create = require('../../handlers/addDbGuild');
+
+client.on("guildCreate", async (guild) => {
+    try {
+        const guildId = guild.id
+        // Call the add and check functions
+        await create.addDbGuild(guildId);
+        // Display in the terminal information
+        console.log(
+            chalk.gray('—————————————————') +
+            chalk.white('[') + chalk.red('Dodawanie servera') + chalk.white(']') + 
+            chalk.gray('—————————————————')
+        );
+        // Display in the terminal information about the addition of
+        console.log(chalk.gray('Dodano nowy server: ') + chalk.red.underline.bold(`${guild.name} || ${guild.id}`));
+
+    } catch (err) {
+        console.log(err);
+    };
+});
+/**
+ * 
+ * @INFO
+ * Bot Coded by ZabKoz#2744
+ * @INFO
+ * Please mention me when you use this code!
+ *
+ */
---
+++ handlers/addDbGuild.js
@@ -0,0 +1,40 @@
+// ———————————————[Packages]———————————————
+const { dbConnect } = require('./dbConnection.js');
+const chalk = require('chalk');
+
+let conn
+conn = dbConnect()
+
+module.exports = {
+    // Function responsible for checking if a server exists and adding it
+    addDbGuild: (guildId) => {
+        // Checking if a server exists in the guildPrefix table
+        conn.query("SELECT * FROM guildPrefix WHERE guildId = ?", [guildId], (err, res) => {
+            // If the result is 0
+            if (res.length == 0) {
+                // Add a new record to the database
+                conn.query("INSERT INTO guildPrefix (guildId, guildPrefix) VALUES (?, ?)", [guildId, process.env.clientPrefix] );
+                // Display in the terminal information about the addition of
+                console.log(chalk.gray('Dodano: ') + chalk.red.underline.bold(`${guildId} `) + chalk.gray('do tabeli prefix'));
+            };
+        });
+        // Checking if a server exists in the guildSettings table
+        conn.query("SELECT * FROM guildSettings WHERE guildId = ?", [guildId], (err, res1) => {
+            // If the result is 0
+            if (res1.length == 0) {
+                // Add a new record to the database
+                conn.query("INSERT INTO guildSettings (guildId) VALUES (?)", [guildId] );
+                // Display in the terminal information about the addition of
+                console.log(chalk.gray('Dodano: ') + chalk.red.underline.bold(`${guildId} `) + chalk.gray('do tabeli ustawień'));
+            };
+        });
+    },
+};
+/**
+ * 
+ * @INFO
+ * Bot Coded by ZabKoz#2744
+ * @INFO
+ * Please mention me when you use this code!
+ *
+ */
