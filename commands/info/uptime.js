4ed8a5ae044efdf2c9ac0d3fd491d40abc15eb42
ZabKoz <zabkoz@spoko.pl>
2022-02-13 02:49:04 +0100

    ⚙| Create uptime command

---
+++ commands/info/uptime.js
@@ -0,0 +1,37 @@
+const { MessageEmbed } = require('discord.js');
+const ms = require('ms');
+const { embedOk } = require('../../config/color.json');
+const { uptime } = require('../../config/emoji.json');
+
+module.exports = {
+   name: "uptime",
+   aliases: ["up"],
+   description: "Czas działania",
+   botpermissions: ["ADMINISTRATOR"],
+   usage: " ",
+   cooldowns: 2000,
+   premiumOnly: false,
+   developersOnly: false,
+   toggleOff: false,
+   run: async (client, message, args) => {
+      message.delete();
+
+    let uptime_embed = new MessageEmbed()
+        .setColor(embedOk)
+        .setTitle('Sprawdzanie...')
+        .setDescription(`${uptime}| \`${ms(client.uptime)}\``)
+        .setTimestamp()
+        .setFooter({ text: `${process.env.clientName} -> ${message.author.tag}`, iconURL: `${process.env.clientAvatar}` });
+    message.channel.send({ embeds: [uptime_embed] }).then(msg => {
+        setTimeout(() => msg.delete(), 60000)
+    });
+   },
+};
+/**
+ * 
+ * @INFO
+ * Bot Coded by ZabKoz#2744
+ * @INFO
+ * Please mention me when you use this code!
+ *
+ */
