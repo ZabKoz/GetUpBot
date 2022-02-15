8ebeeba1eb5af65b38b08f6a0cf8cb95b35f7e45
ZabKoz <zabkoz@spoko.pl>
2022-02-15 02:06:54 +0100

    ⚙| Added meme command

---
+++ commands/fun/meme.js
@@ -0,0 +1,59 @@
+const request = require('node-superfetch');
+const { MessageEmbed } = require('discord.js');
+const { embedOk, embedError } = require('../../config/color.json');
+const { circleno, meme, meme1, meme2, meme3 } = require('../../config/emoji.json');
+
+module.exports = {
+   name: "meme",
+   //aliases: ["..."],
+   description: "Memy",
+   botpermissions: ["ADMINISTRATOR"],
+   usage: " ",
+   cooldowns: 2000,
+   premiumOnly: false,
+   developersOnly: false,
+   toggleOff: false,
+   run: async (client, message, args) => {
+      message.delete();
+      try {
+        var Responses = [
+            "Polska_wpz",
+            "MemyPolskaa",
+            "polishfunny"
+        ];
+        const Response = Math.floor(Math.random() * Responses.length);
+        const { body } = await request
+            .get(`https://www.reddit.com/r/${Responses[Response]}.json?sort=top&t=week`)
+            .query({
+                limit: 800
+            });
+        const allowed = body.data.children;
+        const randomnumber = Math.floor(Math.random() * allowed.length);
+        let Nsfw_embed = new MessageEmbed()
+            .setColor(embedOk)
+            .setTitle(`| Meme || ${Responses[Response]}`)
+            .setDescription(`Tytuł: ${allowed[randomnumber].data.title}\nWysłał: ${allowed[randomnumber].data.author}`)
+            .setImage(allowed[randomnumber].data.url)
+            .setFooter({ text: `${process.env.clientName} -> ${message.author.tag}`, iconURL: `${process.env.clientAvatar}` })
+        message.channel.send({ embeds: [Nsfw_embed] }).then(embedMessage => {
+            embedMessage.react(meme3)
+            .then(() => embedMessage.react(meme1))
+            .then(() => embedMessage.react(meme2))
+        })
+    } catch (err) {
+        let NoNsfw_embed = new MessageEmbed()
+            .setColor(embedError)
+            .setTitle(`${circleno}| Wystąpił błąd!`)
+        message.channel.send({ embeds: [NoNsfw_embed] })
+        console.log(err);
+    };
+   },
+};
+/**
+ * 
+ * @INFO
+ * Bot Coded by ZabKoz#2744
+ * @INFO
+ * Please mention me when you use this code!
+ *
+ */
